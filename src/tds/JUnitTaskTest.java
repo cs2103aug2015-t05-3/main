823bc0cc4ffb7d136a0d289e9116ba8893fcd50f
amoshydra <amoswwj@gmail.com>
2015-09-24 01:49:18 +0800

    Extract and relocate task attributes and conditions as class attribute

--- src/tds/JUnitTaskTest.java
+++ src/tds/JUnitTaskTest.java
@@ -15,35 +15,36 @@ public class JUnitTaskTest {
 	private static final int MINUTE = 60;
 	private static final int HOUR = 3600;
 
+	// Initializing task attributes
+	private String taskName 		= "Buy Milk tonight";
+	private String taskNameLowCase	= "buy milk tonight";
+	private String taskNameDiff	= "Buy Milk tomorrow";
+
+	private Date nowTime 	= new Date();
+	private long startTime	= nowTime.getTime() % 1000;
+	private long endTime	= startTime + (2 * HOUR);
+	
+	private int flag = Task.FLAG_NULL;
+	
+	// Setting up different task conditions
+	private Task taskOriginal =	new Task(taskName, startTime, endTime, flag);
+	private Task taskSameTask =	new Task(taskName, startTime, endTime, flag);
+	private Task taskLowCase =	new Task(taskNameLowCase, startTime, endTime, flag);
+	private Task taskIsDone =	new Task(taskName, startTime, endTime, Task.FLAG_DONE);
+	private Task taskDiffTime =	new Task(taskNameDiff, startTime, startTime, flag);
+	private Task taskDiffName =	new Task(taskNameDiff, startTime, endTime, flag);
+	
 	@Test
 	public void testEquals() {
-		
-		// Initializing task attributes
-		String taskName = "Buy Milk tonight";
-		String taskNameDiff = "Buy Milk tomorrow";
-
-		Date timeNow = new Date();
-		long startTime = timeNow.getTime() % 1000;
-		long endTime = startTime + (2 * HOUR);
-		
-		int flag = Task.FLAG_NULL;
-		
-		// Setting up different task conditions
-		Task taskOriginal = new Task(taskName, startTime, endTime, flag);
-		Task taskSame = new Task(taskName, startTime, endTime, flag);
-		Task taskLowerCase = new Task(taskName.toLowerCase(), startTime, endTime, flag);
-		Task taskIsDone = new Task(taskName, startTime, endTime, Task.FLAG_DONE);
-		Task taskDiffTime = new Task(taskNameDiff, startTime, startTime, flag);
-		Task taskDiffName = new Task(taskNameDiff, startTime, endTime, flag);
 		
 		// Setting up test cases
 		boolean testEqual, testDiffCase, testDiffName, testDiffTime, testDiffFlag;
 		
-		testEqual = taskOriginal.equals(taskSame);
-		testDiffCase = taskOriginal.equals(taskLowerCase); 
-		testDiffFlag = taskOriginal.equals(taskIsDone);
-		testDiffTime = taskOriginal.equals(taskDiffTime);
-		testDiffName = taskOriginal.equals(taskDiffName);
+		testEqual = 	taskOriginal.equals(taskSameTask);
+		testDiffCase =	taskOriginal.equals(taskLowCase); 
+		testDiffFlag =	taskOriginal.equals(taskIsDone);
+		testDiffTime =	taskOriginal.equals(taskDiffTime);
+		testDiffName =	taskOriginal.equals(taskDiffName);
 			
 		// Evaluate test cases
 		assertTrue(testEqual);
@@ -52,5 +53,4 @@ public class JUnitTaskTest {
 		assertFalse(testDiffTime);
 		assertFalse(testDiffFlag);
 	}
-	
 }
