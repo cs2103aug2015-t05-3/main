765c89ec847156f7f79593f696256df03e7a583c
amoshydra <amoswwj@gmail.com>
2015-09-23 23:07:27 +0800

    Added constructors for task class

--- src/tds/task.java
+++ src/tds/task.java
@@ -4,3 +4,46 @@
  * @author amoshydra
  */
 package tds;
+import java.util.Date;
+
+public class task {
+	private String name;
+	private Date startTime;
+	private Date endTime;
+	private int flag;
+
+	/**
+     * Initializes a newly created {@code Task} object so that it store
+     * the name, starting time, ending time and the flag as the argument.
+     */	
+	public task(String name, Date startTime, Date endTime, int flag) {
+		this.name = name;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.flag = flag;
+	}
+	
+	/**
+     * Initializes a newly created {@code Task} object so that it store
+     * the name, starting time, ending time and the flag as the argument. 
+     * This constructor will accept both starting time and ending time in 
+     * {@code long} format.  
+     */	
+	public task(String name, long startTimeSecond, long endTimeSecond, int flag) {
+		this(
+			name, 
+			new Date(startTimeSecond),
+			new Date(endTimeSecond),
+			flag
+		);
+	}
+	
+	/**
+     * Initializes a newly created {@code Task} object which contain only
+     * the name or description of the task. The value of starting time, ending time and
+     * flag are set to null or zero.
+     */	
+	public task(String name) {
+		this(name, null, null, 0);
+	}
+}
