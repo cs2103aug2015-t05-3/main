a7670b559e3193d74d259969acd04b724523de95
amoshydra <amoswwj@gmail.com>
2015-09-24 17:55:43 +0800

    Add ArrayList implementation of TaskCollection

---
+++ src/tds/TaskArrayList.java
@@ -0,0 +1,85 @@
+package tds;
+
+import java.util.List;
+
+public class TaskArrayList implements TaskCollection<Task> {
+
+	@Override
+	public void buildCollection(List<Task> collection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Task> searchName(String searchTerm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> searchStartTime(long startTimeSearch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> searchEndTime(long endTimeSearch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> searchFlag(int flagSearch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> sortName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> sortStartTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> sortEndTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> sortFlag() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
