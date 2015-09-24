463e67ae8bf008583d7fb6b8619f1126c4296eb8
amoshydra <amoswwj@gmail.com>
2015-09-24 17:21:44 +0800

    Implement skeleton structure for TaskCollection

---
+++ src/tds/TaskArrayList.java
@@ -0,0 +1,61 @@
+/**
+ * @author amoshydra
+ *
+ */
+package tds;
+import java.util.List;
+
+public class TaskArrayList implements TaskCollection<Task> {
+
+	public TaskArrayList() {
+		
+	}
+	
+	@Override
+	public void add(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Task> searchName(String searchTerm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> searchStartTime(long startTimeSearch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> searchEndTime(long endTimeSearch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> searchFlag(int flagSearch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
