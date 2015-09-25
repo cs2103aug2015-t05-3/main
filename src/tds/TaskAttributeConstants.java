d6127945cff7d08675feacc37b516652b5e2a7ed
amoshydra <amoswwj@gmail.com>
2015-09-25 16:57:15 +0800

    Maintain a list of constant for task attributes

---
+++ src/tds/TaskAttributeConstants.java
@@ -0,0 +1,17 @@
+package tds;
+
+public class TaskAttributeConstants {
+
+	public final static int NAME = 0;
+	public final static String NAME_TYPE_STRING = "0";
+
+	public final static int START_TIME = 1;
+
+	public final static int END_TIME = 2;
+
+	public final static int FLAG = 3;
+
+	public final static int PRIORITY = 4;
+
+	public final static int CREATE_TIME = 5;
+}
