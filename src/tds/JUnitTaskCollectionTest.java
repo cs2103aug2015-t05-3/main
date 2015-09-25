dc6b9edde2792bc0a85692610046b714f7401694
amoshydra <amoswwj@gmail.com>
2015-09-25 14:51:02 +0800

    Add snippet to test replace method

--- src/tds/JUnitTaskCollectionTest.java
+++ src/tds/JUnitTaskCollectionTest.java
@@ -6,6 +6,9 @@
 
 package tds;
 import org.junit.Test;
+
+import tds.comparators.*;
+
 import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 
@@ -44,10 +47,10 @@ public class JUnitTaskCollectionTest {
 			Task.FLAG_NULL
 			};
 	private int[] priorities = {
+			Task.PRIORITY_NORMAL,
 			Task.PRIORITY_HIGH,
 			Task.PRIORITY_NORMAL,
-			Task.PRIORITY_NORMAL,
-			Task.PRIORITY_NORMAL,
+			Task.PRIORITY_VERY_HIGH,
 			Task.PRIORITY_NORMAL
 			};
 	
@@ -64,27 +67,166 @@ public class JUnitTaskCollectionTest {
 		
 		// Smaller than first index
 		testValue = endTimes[FIRST_ELEMENT] - FIVE_SECONDS;
-		returnIndex = TaskArrayList.getClosestMatchIndex(list, testValue, FIRST_ELEMENT, LAST_ELEMENT);
+		returnIndex = TaskArrayList.getClosestMatchIndex(list, testValue,
+				Task.GET_VALUE_END_TIME, FIRST_ELEMENT, LAST_ELEMENT);
 		assertTrue(returnIndex == FIRST_ELEMENT);
 		
 		// Exact match is found
 		for (int i = FIRST_ELEMENT; i < NUM_OF_ITEMS; i++) {
 			testValue = endTimes[i];
-			returnIndex = TaskArrayList.getClosestMatchIndex(list, testValue, FIRST_ELEMENT, LAST_ELEMENT);
+			returnIndex = TaskArrayList.getClosestMatchIndex(list, testValue,
+					Task.GET_VALUE_END_TIME, FIRST_ELEMENT, LAST_ELEMENT);
 			assertTrue(returnIndex == i);
 		}
 		
 		// Checking value between i and i+1 should always return i+1
 		for (int i = FIRST_ELEMENT; i < NUM_OF_ITEMS; i++) {
 			testValue = endTimes[i] + FIVE_SECONDS;
-			returnIndex = TaskArrayList.getClosestMatchIndex(list, testValue, FIRST_ELEMENT, LAST_ELEMENT);
+			returnIndex = TaskArrayList.getClosestMatchIndex(list, testValue,
+					Task.GET_VALUE_END_TIME, FIRST_ELEMENT, LAST_ELEMENT);
 			assertTrue(returnIndex == i + 1);
 		}
 		
 		// Greater than last index
 		testValue = endTimes[LAST_ELEMENT] + FIVE_SECONDS;
-		returnIndex = TaskArrayList.getClosestMatchIndex(list, testValue, FIRST_ELEMENT, LAST_ELEMENT);
+		returnIndex = TaskArrayList.getClosestMatchIndex(list, testValue,
+				Task.GET_VALUE_END_TIME, FIRST_ELEMENT, LAST_ELEMENT);
 		assertTrue(returnIndex == NUM_OF_ITEMS);
 		
+	}
+
+	@Test
+	public void testGetSortedList() {
+		ArrayList<Task> resultList;
+		TaskArrayList list = new TaskArrayList();
+		
+		for (int i = 0; i < NUM_OF_ITEMS; i++) {
+			list.add(new Task(names[i], startTimes[i], endTimes[i], flags[i], priorities[i]));
+		}
+		
+		// Setting up test cases
+		boolean isNameSorted, isStartTimeSorted, isEndTimeSorted, isFlagSorted, isPrioritySorted;
+		
+		resultList = new ArrayList<Task>(list.getSortedList(new NameComparator()));
+		isNameSorted = checkIsAccending(resultList, Task.GET_VALUE_NAME);
+
+		resultList = new ArrayList<Task>(list.getSortedList(new StartTimeComparator()));
+		isStartTimeSorted = checkIsAccending(resultList, Task.GET_VALUE_START_TIME);
+		
+		resultList = new ArrayList<Task>(list.getSortedList(new EndTimeComparator()));
+		isEndTimeSorted = checkIsAccending(resultList, Task.GET_VALUE_END_TIME);
+		
+		resultList = new ArrayList<Task>(list.getSortedList(new FlagComparator()));
+		isFlagSorted = checkIsAccending(resultList, Task.GET_VALUE_FLAG);
+		
+		resultList = new ArrayList<Task>(list.getSortedList(new PriorityComparator()));
+		isPrioritySorted = checkIsAccending(resultList, Task.GET_VALUE_PRIORITY);
+		
+		// Evaluate tests
+		assertTrue(isNameSorted);
+		assertTrue(isStartTimeSorted);
+		assertTrue(isEndTimeSorted);
+		assertTrue(isFlagSorted);
+		assertTrue(isPrioritySorted);
+	}
+	
+	private boolean checkIsAccending(ArrayList<Task> resultList, int option) {
+		boolean isAccending = true;
+		for (int i = FIRST_ELEMENT + 1; i < NUM_OF_ITEMS; i++) {
+			Task first = resultList.get(i - 1);
+			Task second = resultList.get(i);
+			if (first.getValue(option) > second.getValue(option)) {
+				isAccending = false;
+			}
+		}
+		return isAccending;
+	}
+	
+	private boolean checkIsAccending(ArrayList<Task> resultList, long option) {
+		boolean isAccending = true;
+		for (int i = FIRST_ELEMENT + 1; i < NUM_OF_ITEMS; i++) {
+			Task first = resultList.get(i - 1);
+			Task second = resultList.get(i);
+			if (first.getValue(option) > second.getValue(option)) {
+				isAccending = false;
+			}
+		}
+		return isAccending;
+	}
+
+	private boolean checkIsAccending(ArrayList<Task> resultList, String option) {
+		boolean isAccending = true;
+		for (int i = FIRST_ELEMENT + 1; i < NUM_OF_ITEMS; i++) {
+			Task first = resultList.get(i - 1);
+			Task second = resultList.get(i);
+			if (first.getValue(option).compareTo(second.getValue(option)) > 0) {
+				isAccending = false;
+			}
+		}
+		return isAccending;
+	}
+	
+	@Test
+	public void testQueryTime() {
+		TaskArrayList list = new TaskArrayList();
+		ArrayList<Task> resultList;
+		
+		for (int i = 0; i < NUM_OF_ITEMS; i++) {
+			list.add(new Task(names[i], startTimes[i], endTimes[i], flags[i], priorities[i]));
+		}
+		
+		long lowerBound;
+		long upperBound;
+		
+		// Setting up test cases
+		// first item (inclusive) to more than last element (exclusive)
+		lowerBound = endTimes[FIRST_ELEMENT];
+		upperBound = endTimes[LAST_ELEMENT] + FIVE_SECONDS;
+		resultList = new ArrayList<Task>(list.queryEndTime(upperBound, lowerBound));
+		assertTrue(resultList.size() == NUM_OF_ITEMS);
+		
+		// first item (inclusive) to last element (exclusive)
+		lowerBound = endTimes[FIRST_ELEMENT];
+		upperBound = endTimes[LAST_ELEMENT];
+		resultList = new ArrayList<Task>(list.queryEndTime(upperBound, lowerBound));
+		assertTrue(resultList.size() == NUM_OF_ITEMS - 1);
+		
+		// lowerBound > upperBound
+		lowerBound = endTimes[LAST_ELEMENT];
+		upperBound = endTimes[FIRST_ELEMENT];
+		resultList = new ArrayList<Task>(list.queryEndTime(upperBound, lowerBound));
+		assertTrue(resultList.size() == 0);
+		
+		// item less than first element (inclusive) to first item (exclusive)
+		lowerBound = endTimes[LAST_ELEMENT] - FIVE_SECONDS;
+		upperBound = endTimes[LAST_ELEMENT];
+		resultList = new ArrayList<Task>(list.queryEndTime(upperBound, lowerBound));
+		assertTrue(resultList.size() == 0);
+	}
+
+	public void testReplace() {
+		TaskArrayList list = new TaskArrayList();
+		ArrayList<Task> resultList;
+		
+		for (int i = 0; i < NUM_OF_ITEMS; i++) {
+			list.add(new Task(names[i], startTimes[i], endTimes[i], flags[i], priorities[i]));
+		}
+		
+		resultList = new ArrayList<Task>(list.searchName("o"));
+		for (Task task : resultList) {
+			System.out.println(task);
+		}
+		System.out.println();
+		
+		Task temp = resultList.get(2);
+		temp.setFlag(Task.FLAG_DONE);
+		temp.setName("This is cool");
+		
+		for (Task task : resultList) {
+			System.out.println(task);
+		}
+		System.out.println();
+		
+		System.out.println(list);
 	}
 }
