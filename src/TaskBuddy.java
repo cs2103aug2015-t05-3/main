412d7337d40920ebad7fb46cb3ccbb5f3d626b34
yancmo <yancm212@yahoo.com.sg>
2015-09-29 04:33:51 +0800

    Added minor workings to skeleton of TaskBuddy. Added ParsedCommand.

--- src/TaskBuddy.java
+++ src/TaskBuddy.java
@@ -4,6 +4,8 @@
  * @author Yan Chan Min Oo
  */
 
+import java.util.Scanner;
+
 import command.*;
 import tds.TaskTree;
 
@@ -13,6 +15,11 @@ public class TaskBuddy {
 	 * Constants 
 	 */
 	private static final String configFileName = "config.xml";
+	
+	/*
+	 * Global variables
+	 */
+	private static Scanner _in;
 
 	public static void main(String[] args) {
 		// Load arguments from config file
@@ -25,6 +32,18 @@ public class TaskBuddy {
 		// Load GUI (NEXT TIME, IGNORE FOR NOW)
 		
 		// (Loop) Execute commands
+		runCommands();
+	}
+	
+	private static void runCommands(){
+		String cmd;
+		do{
+			cmd = getInput();
+		} while (!cmd.equals("exit"));
+	}
+	
+	private static String getInput(){
+		return _in.nextLine();
 	}
 
 }
