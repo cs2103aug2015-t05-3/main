ac72eb3691212ee6aa39420df26d149895f5cfa1
Zander <Zander@Desktop>
2015-09-29 20:25:58 +0800

    Modified TimeUtil.java
    Added several methods to allows us to convert formatted date to long
    time and vice versa.

--- src/util/TimeUtil.java
+++ src/util/TimeUtil.java
@@ -5,27 +5,57 @@
  */
 package util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TimeUtil {
 
+	private static SimpleDateFormat _df = new SimpleDateFormat("EEEE: dd/MM/yy HH:mm 'GMT'Z");
 	/**
 	 * Converts a system time in string to long format.
+	 * 
 	 * @return System time in long format if the string is valid
 	 */
-	public static long StringToLongTime(String sysTimeInString){
-		try{
+	public static long StringToLongTime(String sysTimeInString) {
+		try {
 			return Integer.parseInt(sysTimeInString);
-		} catch (NumberFormatException e){
+		} catch (NumberFormatException e) {
 			return 0;
 		}
 	}
-	
+
 	/**
 	 * Returns date format in String
 	 */
-	public static String getDate(long time){
+	public static String getDate(long time) {
 		Date date = new Date(time * 1000);
 		return date.toString();
+	}
+
+	/**
+	 * 
+	 * Converts a long time to date format
+	 * @return date in sample format: Tuesday: 29/09/15 20:15 GMT+0800
+	 */
+	public static String getFormattedDate(long time) {
+		Date date = new Date(time);
+		String dateText = _df.format(date);
+		return dateText;
+	}
+	
+	/**
+	 * Converts a date to long time
+	 * ToDo: Refactor this
+	 */
+	public static long getLongTime(String sDate) {
+		try {
+			Date date = (Date)_df.parse(sDate);
+			return date.getTime();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
 	}
 }
