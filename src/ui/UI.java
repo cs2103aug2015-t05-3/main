4df910c1cc9a59bca55e4f5a95ef545f6ca7e266
amoshydra <amoswwj@gmail.com>
2015-10-19 21:50:57 +0800

    Add JavaFx UI launch source code

---
+++ src/ui/UI.java
@@ -0,0 +1,36 @@
+package ui;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+
+public class UI extends Application {
+    
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) {
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("UI.fxml"));
+			
+			Scene scene = new Scene(root);
+	        stage.setTitle("TaskBuddy v0.2");
+	        stage.getIcons().add(new Image(getClass().getResourceAsStream("bal.png")));
+	        stage.setScene(scene);
+	        stage.setResizable(false);
+	        stage.show();
+	        
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
