58f89906bb20fefc09fd3f05867b1d45c41cc496
amoshydra <amoswwj@gmail.com>
2015-10-25 23:29:46 +0800

    Fix problem displaying command message

--- src/ui/UIController.java
+++ src/ui/UIController.java
@@ -9,8 +9,6 @@ import constants.UIFieldIndex;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -20,7 +18,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
-import parser.CommandProcessor;
 import taskCollections.Task;
 import util.TimeUtil;
 
@@ -31,7 +28,8 @@ public class UIController implements Initializable {
 	 */
 
 	@FXML
-	private static Text cmdMsg;
+	private Text cmdMsg;
+	private static Text cmdMsgClone;
 	@FXML
 	private TableColumn<UITask1, String> id1;
 	@FXML
@@ -52,14 +50,6 @@ public class UIController implements Initializable {
 	private TableView<UITask2> table2 = new TableView<UITask2>();
 	@FXML
 	private TextField input = new TextField();
-	@FXML
-	private TextField input2 = new TextField();
-	@FXML
-	private TextField input3 = new TextField();
-	@FXML
-	private TextField input4 = new TextField();
-	@FXML
-	private TextField input5 = new TextField();
 
 	private int troll = 100;
 
@@ -72,30 +62,23 @@ public class UIController implements Initializable {
 
 	final static ObservableList<UITask1> data = FXCollections.observableArrayList();
 	final static ObservableList<UITask2> data1 = FXCollections.observableArrayList();
-	
-	private static final double WIDTH_EXPANDED = 4080;
-	private static final double WIDTH_SHRINKED = 250;
 
 	public void enterPressed() {
 		//String in = input.getText().trim();
-		
+
 		synchronized (inputBuffer) {
             String[] in = new String[UIFieldIndex.INPUT_BUFFSIZE];
             // TODO: Convert to array
             in[0] = input.getText().trim();
-            in[1] = input2.getText().trim();
-            in[2] = input3.getText().trim();
-            in[3] = input4.getText().trim();
-            in[4] = input5.getText().trim();
-            		
+
 			inputBuffer.add(in);
             inputBuffer.notify();
         }
-		
+
 		// Other classes will do the job.
 		clearInput();
 	}
-	
+
 	public static String[] getInput() {
 		synchronized (inputBuffer) {
             // wait for input from field
@@ -112,7 +95,7 @@ public class UIController implements Initializable {
 	public void add() {
 		UITask1 t = new UITask1(troll--, "Buy Milk from Shop", 1442851200000L, 1443283140000L);
 		data.add(t);
-		
+
 		UITask2 t1 = new UITask2(troll--, "Go Die in a fire", "N");
 		data1.add(t1);
 		table1.getSortOrder().add(id1);
@@ -139,19 +122,15 @@ public class UIController implements Initializable {
 			//lock = new Semaphore(1);
 		}
 	}
-	
+
 	public void clearInput(){
 		input.clear();
-		input2.clear();
-		input3.clear();
-		input4.clear();
-		input5.clear();
 	}
-	
+
 	public static void seperateTaskList(List<Task> taskList){
 		_nonFloatingTaskList = new ArrayList<Task>();
 		_floatingTaskList = new ArrayList<Task>();
-			
+
 		//Iterate through list and remove all floating tasks
 		for(int i=0; i < taskList.size();){
 			if(isFloating(taskList.get(i))){
@@ -162,7 +141,7 @@ public class UIController implements Initializable {
 		}
 		//Remaining tasks are all non-floating
 		_nonFloatingTaskList = taskList;
-		
+
 		genTable();
 	}
 
@@ -173,106 +152,52 @@ public class UIController implements Initializable {
 			return false;
 		}
 	}
-	
+
 	private static void genTable() {
 		data.clear();
 		data1.clear();
-		
+
 		for (Task t : _nonFloatingTaskList) {
 			UITask1 ui1 = new UITask1(t.getId(), t.getName(), t.getStartTime(), t.getEndTime());
 			data.add(ui1);
 		}
-		
+
 		for (Task t : _floatingTaskList) {
 			//TODO: RESOLVE PRIORITY
 			UITask2 ui2 = new UITask2(t.getId(), t.getName(), "N");
 			data1.add(ui2);
 		}
 	}
-	
+
 	public static void setOutputMsg(String a){
-		//cmdMsg.setText(a);
+		try {
+			cmdMsgClone.setText(a);
+		} catch (NullPointerException e) {
+			return;
+		}
 	}
 
 	@Override
-	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {				
-				
+	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+
+		cmdMsgClone = cmdMsg;
+		cmdMsgClone.setText("");
+
 		id1.setCellValueFactory(new PropertyValueFactory<UITask1, String>("id"));
 		task1.setCellValueFactory(new PropertyValueFactory<UITask1, String>("Task"));
 		sDate.setCellValueFactory(new PropertyValueFactory<UITask1, String>("sDate"));
 		eDate.setCellValueFactory(new PropertyValueFactory<UITask1, String>("eDate"));
-		
+
 		id2.setCellValueFactory(new PropertyValueFactory<UITask2, String>("id"));
 		task2.setCellValueFactory(new PropertyValueFactory<UITask2, String>("Task"));
 		priority.setCellValueFactory(new PropertyValueFactory<UITask2, String>("priority"));
 
 		id1.setSortType(TableColumn.SortType.ASCENDING);
-		
+
 		table1.setItems(data);
 		table2.setItems(data1);
-		
+
 		table1.getSortOrder().add(id1);
-		
-		/*input2.focusedProperty().addListener(new ChangeListener<Boolean>() {
-			@Override
-			public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue,
-					Boolean newPropertyValue) {
-				if (newPropertyValue) {
-					System.out.println("Textfield on focus");
-					input.setPrefWidth(250);
-				}
-			}
-		});
-		input.focusedProperty().addListener(new ChangeListener<Boolean>() {
-			@Override
-			public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue,
-					Boolean newPropertyValue) {
-				if (newPropertyValue) {
-					System.out.println("Textfield out focus");
-					input.setPrefWidth();
-				}
-			}
-		});*/
-
-		input.textProperty().addListener(new ChangeListener<String>() {
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-
-				if (isValidCmd(input.getText().trim())) {
-					shrinkCmdCell();
-				} else {
-					expandCmdCell();
-				}
-			}
-		});
-	}
-	
-	private void expandCmdCell(){
-		input.setPrefWidth(WIDTH_EXPANDED);
-		hideRedundantCells();
-	}
-	
-	private void shrinkCmdCell(){
-		input.setPrefWidth(WIDTH_SHRINKED);
-		showRedundantCells();
-	}
-	
-	private void hideRedundantCells(){
-		input2.setVisible(false);
-		input3.setVisible(false);
-		input4.setVisible(false);
-		input5.setVisible(false);
-	}
-	
-	private void showRedundantCells(){
-		input2.setVisible(true);
-		input3.setVisible(true);
-		input4.setVisible(true);
-		input5.setVisible(true);
-	}
-	
-	private boolean isValidCmd(String input){
-		return CommandProcessor.getInstance().getEffectiveCmd(input) == null ? false : true;
 	}
 
 	// To Implement Priority
@@ -280,13 +205,13 @@ public class UIController implements Initializable {
 		private final SimpleIntegerProperty id;
 		private final SimpleStringProperty task;
 		private final SimpleStringProperty priority;
-		
+
 		private UITask2(int id, String task, String priority) {
 			this.id = new SimpleIntegerProperty(id);
 			this.task = new SimpleStringProperty(task);
 			this.priority = new SimpleStringProperty(priority);
 		}
-		
+
 		public int getId() {
 			return id.get();
 		}
@@ -294,12 +219,12 @@ public class UIController implements Initializable {
 		public String getTask() {
 			return task.get();
 		}
-		
+
 		public String getPriority() {
 			return priority.get();
 		}
 	}
-	
+
 	public static class UITask1 {
 		private final SimpleIntegerProperty id;
 		private final SimpleStringProperty task;
@@ -322,15 +247,15 @@ public class UIController implements Initializable {
 		public String getTask() {
 			return task.get();
 		}
-		
+
 		public String getSDate() {
 			return sDate.get();
 		}
-		
+
 		public String getEDate() {
 			return eDate.get();
 		}
-		
+
 		public long getEDateLong() {
 			return eDateLong.get();
 		}
--- src/ui/UIHelper.java
+++ src/ui/UIHelper.java
@@ -1,73 +1,62 @@
 /**
  * Provides methods for classes to interact with UIFrame
- * 
+ *
  * @author Yan Chan Min Oo
  */
 package ui;
 
-
 import java.util.List;
-import java.util.logging.Logger;
-
-import javax.swing.JOptionPane;
-
 import taskCollections.Task;
 
-
 public class UIHelper {
-	
+
 	/*
-	private static UIFrame frame;
-	private static Logger log = Logger.getLogger("log");
-	*/
-	
-	//private static UI user_interface;
-	
+	 * private static UIFrame frame;
+	 */
+	// private static Logger log = Logger.getLogger("log");
+	// private static UI user_interface;
+
 	/**
 	 * Returns a string of the user input. Triggered on enter press.
 	 */
-	
-	public static String[] getUserInput(){
+
+	public static String[] getUserInput() {
 		return UIController.getInput();
 	}
-	
-	
+
 	public static void loadTask() {
-		
+
 	}
-	
+
 	public static void createUI() {
 		UIController.createUI();
 	}
-	
-	
-	
+
 	/**
 	 * Destroys the UI
 	 */
-	
-	public static void destroyUI(){
-		//frame.dispose();
+
+	public static void destroyUI() {
+		// frame.dispose();
 	}
-	
-	public static void appendOutput(String appendString){
-		//frame.setOutputText(frame.getOutputText() + appendString + System.lineSeparator());
+
+	public static void appendOutput(String appendString) {
+		// frame.setOutputText(frame.getOutputText() + appendString +
+		// System.lineSeparator());
 	}
-	
-	//give the entire list for output generation
-	public static void setTableOutput(List<Task> completeList){
+
+	// give the entire list for output generation
+	public static void setTableOutput(List<Task> completeList) {
 		UIController.seperateTaskList(completeList);
 	}
-	
-	//set UI's output msg
+
+	// set UI's output msg
 	public static void setOutputMsg(String s) {
-		JOptionPane.showMessageDialog(null, s);
 		UIController.setOutputMsg(s);
 	}
-	
-	
-	public static void setUserInput(String in){
-		//frame.setInputText(in);
+
+	public static void setUserInput(String in) {
+		// frame.setInputText(in);
 	}
-	
+
 }
