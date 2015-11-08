93b948ed1e9bc05aed3464a50451128fb619d10b
amoshydra <amoswwj@gmail.com>
2015-11-08 16:49:46 +0800

    Do refactoring on log formatter and change the logging format

--- src/logger/LogFormatter.java
+++ src/logger/LogFormatter.java
@@ -9,25 +9,51 @@ import java.util.logging.LogRecord;
 
 public class LogFormatter extends Formatter {
 
-	private static final DateFormat df = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss.SSS");
+	// Log formatting head/tail constants
+	private static final String HEADTAIL_SEPARATOR = "\n";
+	private static final String HEADTAIL_PADDER = "********";
+	private static final String HEAD_MSG = " Log start ";
+	private static final String TAIL_MSG = "  Log end  ";
+
+
+	// Log formatting body constants
+	private static final String CLASS_OPEN = "[";
+	private static final String CLASS_CLOSE = "]";
+	private static final String CLASS_METHOD_DELIMETER = ".";
+	private static final String LEVEL_OPEN = "[";
+	private static final String LEVEL_CLOSE = "]";
+	private static final String ATTRIBUTES_SEPARTOR = " - ";
+	private static final String LOG_SEPARATOR = "\n";
+
+	// Date format constant
+	private static final String DATE_FORMAT_FULL = "yyyy.MM.dd hh:mm:ss.SSS";
+	private static final DateFormat df = new SimpleDateFormat(DATE_FORMAT_FULL);
 
 	@Override
 	public String format(LogRecord record) {
-		StringBuilder builder = new StringBuilder(1000);
-		builder.append(df.format(new Date(record.getMillis()))).append(" - ");
-		builder.append("[").append(record.getSourceClassName()).append(".");
-		builder.append(record.getSourceMethodName()).append("] - ");
-		builder.append("[").append(record.getLevel()).append("] - ");
-		builder.append(formatMessage(record));
-		builder.append("\n");
-		return builder.toString();
+
+		String dateStr = df.format(new Date(record.getMillis()));
+		String classStr = record.getSourceClassName();
+		String methodStr = record.getSourceMethodName();
+		String levelStr = String.format("%-7s", record.getLevel().toString());
+		String message = formatMessage(record);
+
+		String logBody = dateStr + ATTRIBUTES_SEPARTOR +
+				LEVEL_OPEN + levelStr + LEVEL_CLOSE + ATTRIBUTES_SEPARTOR +
+				CLASS_OPEN + classStr + CLASS_METHOD_DELIMETER + methodStr + CLASS_CLOSE + ATTRIBUTES_SEPARTOR +
+				message + LOG_SEPARATOR;
+		return logBody;
 	}
 
 	public String getHead(Handler h) {
-		return super.getHead(h);
+		String logHead = HEADTAIL_PADDER + HEAD_MSG + HEADTAIL_PADDER
+				+ HEADTAIL_SEPARATOR;
+		return logHead;
 	}
 
 	public String getTail(Handler h) {
-		return super.getTail(h);
+		String logTail = HEADTAIL_PADDER + TAIL_MSG + HEADTAIL_PADDER +
+				HEADTAIL_SEPARATOR;
+		return logTail;
 	}
 }
