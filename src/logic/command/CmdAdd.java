60c4dee48984b6161ccefbb0c735170fe1637841
yancmo <yancm212@yahoo.com.sg>
2015-11-09 15:06:07 +0800

    Updated documentation for add

--- src/logic/command/CmdAdd.java
+++ src/logic/command/CmdAdd.java
@@ -1,6 +1,6 @@
 /**
  * The add command
- *
+ * 
  * @author Yan Chan Min Oo
  */
 package logic.command;
@@ -18,20 +18,12 @@ public class CmdAdd extends Command {
 	 * Constants
 	 */
 	// Message constants
-	// private static final String MSG_TASKUNSPECIFIED = "Please specify a task
-	// name";
 	private static final String MSG_TASKADDED = "Added : %1$s";
 	private static final String MSG_TASKNAMENOTGIVEN = "Please enter a task name";
 	private static final String MSG_STARTAFTEREND = "Specified start time should be before end time";
-	
-	//Help Info
-	private static final String HELP_INFO_ADD = 
-			"<task_name> [%1$s <start_time>] [%2$s <end_time>] [%2$s <end_time>] [%3$s high/normal/low/h/n/l]";
 
-	// Error codes
-	/*
-	 * private enum ERROR { OK, TASKUNSPECIFIED }
-	 */
+	// Help Info
+	private static final String HELP_INFO_ADD = "<task_name> [%1$s <start_time>] [%2$s <end_time>] [%2$s <end_time>] [%3$s high/normal/low/h/n/l]";
 
 	/*
 	 * Variables for internal use
@@ -42,21 +34,13 @@ public class CmdAdd extends Command {
 	private String taskEndTime;
 	private String taskPriority;
 
-	public CmdAdd() {
-
-	}
-
-	public CmdAdd(String taskName) {
-		this.taskName = taskName;
-	}
-
 	@Override
 	public CommandAction execute() {
 
-		addTask = getTask(); // Tries to get the task object (if available)
+		addTask = getTask(); // Tries to get the task object (if available). Usually not null if this is called from an undo
 
-		if (addTask == null) { // Not an existing task. Create a task object
-								// from scratch
+		if (addTask == null) { // Not an existing task. Create a task object from scratch
+
 			// Get details of the task to add
 			if (taskName == null) {
 				taskName = getParameterValue(CmdParameters.PARAM_NAME_TASK_NAME);
@@ -68,31 +52,25 @@ public class CmdAdd extends Command {
 			taskEndTime = getParameterValue(CmdParameters.PARAM_NAME_TASK_ENDTIME);
 			taskPriority = getParameterValue(CmdParameters.PARAM_NAME_TASK_PRIORITY);
 
-			/*
-			 * ERROR errCode = validateParam(); // Validate commands if (errCode
-			 * != ERROR.OK) { switch (errCode) { case TASKUNSPECIFIED: return
-			 * MSG_TASKUNSPECIFIED; } }
-			 */
+			assignDefaults(); // Assign defaults for unspecified fields
 
-			assignDefaults(); // Assign defaults
-
-			// Convert the start/end times to a format we can use for
-			// calculation
+			// Convert the start/end times to a format we can use for calculation
 			long taskStartTimeL = TimeUtil.sysStringToLongTime(taskStartTime);
 			long taskEndTimeL = TimeUtil.sysStringToLongTime(taskEndTime);
-			
-			if(!isValidTime(taskStartTimeL, taskEndTimeL)){
+
+			// Ensure the dates given are valid
+			if (!isValidTime(taskStartTimeL, taskEndTimeL)) {
 				return new CommandAction(MSG_STARTAFTEREND, false, _taskTree.searchFlag(FLAG_TYPE.NULL));
 			}
 
 			// Convert the priority to a format we can use for storing
 			PRIORITY_TYPE taskPriorityP = getPriorityValue(taskPriority);
 
-			addTask = new Task(taskName,null, taskStartTimeL, taskEndTimeL, Task.FLAG_TYPE.NULL, taskPriorityP);
+			addTask = new Task(taskName, taskStartTimeL, taskEndTimeL, Task.FLAG_TYPE.NULL, taskPriorityP);
 		}
 
-		_taskTree.add(addTask);
-		
+		_taskTree.add(addTask); // Add the task to the collection
+
 		return new CommandAction(String.format(MSG_TASKADDED, taskName), true, _taskTree.searchFlag(FLAG_TYPE.NULL));
 	}
 
@@ -103,30 +81,22 @@ public class CmdAdd extends Command {
 		return delete.execute();
 	}
 
-	/*
-	 * Validates the parameters which are to be added to the task
-	 */
-	/*
-	 * private ERROR validateParam() { if (taskName == null) { return
-	 * ERROR.TASKUNSPECIFIED; } return ERROR.OK; }
-	 */
-
 	/**
 	 * Assign default values to unspecified parameters
 	 */
 	private void assignDefaults() {
-		if (taskStartTime == null) {
+		if (taskStartTime == null) { // Default start time
 			taskStartTime = "" + Task.DATE_NULL;
 		}
-		if (taskEndTime == null) {
+		if (taskEndTime == null) { // Default end time
 			taskEndTime = "" + Task.DATE_NULL;
 		}
-		if (taskPriority == null) {
+		if (taskPriority == null) { // Default priority
 			taskPriority = CmdParameters.PARAM_VALUE_TASK_PRIORITY_NORM;
 		}
 	}
 
-	/*
+	/**
 	 * Converts the priority from string to a format suitable for storage
 	 */
 	private PRIORITY_TYPE getPriorityValue(String priority) {
@@ -139,9 +109,18 @@ public class CmdAdd extends Command {
 			return PRIORITY_TYPE.NORMAL;
 		}
 	}
-	
-	private boolean isValidTime(long startTime, long endTime){
-		if(endTime == 0 && startTime == 0){
+
+	/**
+	 * Checks if the start time is before the end time
+	 * @param startTime
+	 * 			The start time
+	 * @param endTime
+	 * 			The end time
+	 * @return
+	 * 			True if the time is valid, false if it isn't
+	 */
+	private boolean isValidTime(long startTime, long endTime) {
+		if (endTime == 0 && startTime == 0) {
 			return true;
 		} else {
 			return TimeUtil.compareMinTime(startTime, endTime) < 0;
@@ -158,9 +137,9 @@ public class CmdAdd extends Command {
 		return new String[] { CmdParameters.PARAM_NAME_TASK_STARTTIME, CmdParameters.PARAM_NAME_TASK_ENDTIME,
 				CmdParameters.PARAM_NAME_TASK_PRIORITY };
 	}
-	
+
 	@Override
-	public String getHelpInfo(){
+	public String getHelpInfo() {
 		return String.format(HELP_INFO_ADD, ParserConstants.TASK_SPECIFIER_STARTTIME,
 				ParserConstants.TASK_SPECIFIER_ENDTIME, ParserConstants.TASK_SPECIFIER_PRIORITY);
 	}
